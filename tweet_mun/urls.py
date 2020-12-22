2aa97af4be4dbd19ac157ce6281af9688a8f14f6
Muntaaha <muntaaha.rahman3001@gmail.com>
2020-12-22 22:58:19 +0600

    created-tweeter-list-view

--- tweet_mun/urls.py
+++ tweet_mun/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from tweets.views import home_view
+from tweets.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('tweet_list/', tweet_list_view),
+    path('tweet_detail/<str:tweet_id>/', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,31 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+
+from .models import Tweets
 
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Welcome Muntaaha</h1>")
+
+def tweet_list_view(request,*args,**kwargs):
+    qs = Tweets.objects.all()
+    tweet_list = [{"id":x.id, "content":x.content} for x in qs]
+    data = {
+        "response": tweet_list 
+    }
+    return JsonResponse(data)
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+
+    data = {
+        "id": tweet_id
+    }
+    status = 200
+    try:
+        obj = Tweets.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
