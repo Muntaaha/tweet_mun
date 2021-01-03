0b4823ef34b86231a4bfadae98fc6cbbc0cde000
Muntaaha <muntaaha.rahman3001@gmail.com>
2021-01-03 22:55:56 +0600

    added-likes-field-in-the-databse

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Tweets
-# Register your models here.
+from .models import Tweets, TweetLike
 
-admin.site.register(Tweets)
+# Register your models here.
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
+
+class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
+    list_display = ['__str__','user']
+    search_fields = ['content', 'user__username','user__email']
+    class Meta:
+        model = Tweets
+
+admin.site.register(Tweets, TweetAdmin)
+# admin.site.register(TweetLike, TweetLikeAdmin)
--- tweets/models.py
+++ tweets/models.py
@@ -5,11 +5,19 @@ import random
 User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweets", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweets(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.AutoField(primary_key=True)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     
     class Meta:
