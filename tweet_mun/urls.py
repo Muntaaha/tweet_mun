4ffa0d9cd55e238730242298d712fe4667e63614
Muntaaha <muntaaha.rahman3001@gmail.com>
2021-01-05 07:04:21 +0600

    completed-like-tweet-feature

--- tweet_mun/urls.py
+++ tweet_mun/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from tweets.views import home_view, tweet_list_view, tweet_create_view, tweet_detail_view, tweet_delete_view
+from tweets.views import home_view,tweet_list_view,tweet_create_view,tweet_detail_view,tweet_delete_view,tweet_action_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('tweets/', tweet_list_view),
     path('tweet_create/', tweet_create_view),
     path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
+    path('api/tweets/action', tweet_action_view),
     path('tweet_detail/<str:tweet_id>/', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.permissions import IsAuthenticated
 from .models import Tweets
 from .forms import TweetForm
-from .serializers import TweetSerializer
+from .serializers import TweetSerializer, TweetActionSerializer
 
 # Create your views here.
 
@@ -56,7 +56,27 @@ def tweet_delete_view(request, tweet_id, *args,**kwargs):
     obj.delete()
     return Response({"message": "Successfully Deleted"}, status=200)
 
-
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def tweet_action_view(request, *args,**kwargs):
+    serializer = TweetActionSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        data = serializer.validated_data 
+        tweet_id = data.get("id")
+        action = data.get("action")
+        qs = Tweets.objects.filter(id=tweet_id)
+        if not qs.exists():
+            return Response({}, status=404)
+        obj = qs.first()
+        if action == "like":
+            obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
+        elif action == "unlike":
+            obj.likes.remove(request.user)
+        elif action == "retweet":
+            pass
+    return Response({}, status=200)
 
 
 
